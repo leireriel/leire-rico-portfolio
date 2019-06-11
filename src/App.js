4a780889b748f2bd4872678cff1ddfd199595928
leireriel <leirericop@gmail.com>
2019-06-11 14:55:29 +0200

    header

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <p>hola</p>
         <Header />
         <Hero />
         <Developer />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,16 @@ class Header extends Component {
   render() {
     return (
       <Fragment>
-        <p>header</p>
+        <nav>
+            <ol>
+            <li><a href="/html/">
+              <i class="fas fa-home"></i>
+          </a></li>
+          <li><a href="/css/">Quién soy</a></li>
+          <li><a href="/js/">Proyectos</a></li>
+          <li><a href="/jquery/">Contacto</a></li>
+            </ol>
+        </nav>
       </Fragment>
     );
   }
