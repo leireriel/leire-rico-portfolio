47cc857e463d3a59ed83d8d93aed1bfb50f26249
leireriel <leirericop@gmail.com>
2019-06-11 14:35:23 +0200

    Create structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>footer</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Footer;
