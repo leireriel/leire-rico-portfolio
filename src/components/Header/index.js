e0bcc2be8425dc1015efbc26d0e7bd495baba526
leireriel <leirericop@gmail.com>
2019-06-11 15:25:06 +0200

    Create navigation on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,27 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import './Header.scss';
 
 class Header extends Component {
   render() {
     return (
-      <Fragment>
-        <nav>
-            <ol>
-            <li><a href="/html/">
-              <i class="fas fa-home"></i>
-          </a></li>
-          <li><a href="/css/">Quién soy</a></li>
-          <li><a href="/js/">Proyectos</a></li>
-          <li><a href="/jquery/">Contacto</a></li>
-            </ol>
-        </nav>
-      </Fragment>
+      <nav className="nav__container">
+        <ol className="nav__list">
+          <li className="nav__item">
+            <a href="/html/">
+              <i className="fas fa-home nav__home"></i>
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#..." className="nav__whoIAm">Quién soy</a>
+          </li>
+          <li className="nav__item">
+            <a href="#..." className="nav__projects">Proyectos</a>
+          </li>
+          <li className="nav__item">
+            <a href="#..." className="nav__contact">Contacto</a>
+          </li>
+        </ol>
+      </nav>
     );
   }
 }
