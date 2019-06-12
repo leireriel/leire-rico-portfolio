1ad5e20abad61d3087ffab9376a673e667ca41d0
leireriel <leirericop@gmail.com>
2019-06-12 17:19:52 +0200

    Fix grammar error and update docs folder

--- src/components/Developer/index.js
+++ src/components/Developer/index.js
@@ -17,7 +17,7 @@ class Developer extends Component {
           <h3 className="section__title">Quién soy</h3>
           <p className="section__text">
             ¡Hola! Me llamo Leire y me apasiona la programación <strong>front-end</strong>.
-            A la hora de programar, soy muy curiosa y me gusta probar cosas nuevas y entender cómo funcionan.
+            A la hora de programar, soy muy curiosa y me gusta probar cosas nuevas y entender cómo funcionan. 
             <strong>Me encanta aprender</strong>, ya sea haciendo pair programming o por mi cuenta, y siempre intento <strong>aportar sugerencias</strong>. 
             Tengo gran capacidad de adaptabilidad y me siento muy motivada cuando puedo trabajar en <strong>proyectos nuevos</strong>.
             Además, en mi código voy aplicando todo lo que aprendo sobre <strong>Clean Code :)</strong>
