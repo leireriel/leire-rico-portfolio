99a3d139d32a96c4dd92bcfef138c82071b42fc1
leireriel <leirericop@gmail.com>
2019-06-12 13:59:08 +0200

    Finish contact colors and responsive

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,35 +6,37 @@ class Footer extends Component {
     return (
       <div className="contact__wrapper section__container">
         <h3 className="section__title">Contacto</h3>
+        <p className="section__text contact__description">¡Visítame en mis redes!</p>
         <nav className="contact__container">
           <ol className="contact__list">
             <li className="contact__item">
-              <a href="mailto:leirericop@gmail.com" className="contact__link">
-                <i className="fas fa-envelope"></i>
+              <a href="mailto:leirericop@gmail.com" target="_blank" rel="noopener noreferrer" className="contact__link">
+                <i className="fas fa-envelope contact__link__icon"></i>
               </a>
             </li>
             <li className="contact__item">
-              <a href="https://www.linkedin.com/in/leirerico/" className="contact__link">
-                <i className="fab fa-linkedin-in"></i>
+              <a href="https://www.linkedin.com/in/leirerico/" target="_blank" rel="noopener noreferrer" className="contact__link">
+                <i className="fab fa-linkedin-in contact__link__icon"></i>
               </a>
             </li>
             <li className="contact__item">
-              <a href="https://twitter.com/leireriel1" className="contact__link">
-                <i className="fab fa-twitter"></i>
+              <a href="https://twitter.com/leireriel1" target="_blank" rel="noopener noreferrer" className="contact__link">
+                <i className="fab fa-twitter contact__link__icon"></i>
               </a>
             </li>
             <li className="contact__item">
-              <a href="https://github.com/leireriel/" className="contact__link">
-                <i className="fab fa-github"></i>
+              <a href="https://github.com/leireriel/" target="_blank" rel="noopener noreferrer" className="contact__link">
+                <i className="fab fa-github contact__link__icon"></i>
               </a>
             </li>
             <li className="contact__item">
-              <a href="https://codepen.io/leireriel/" className="contact__link">
-                <i className="fab fa-codepen"></i>
+              <a href="https://codepen.io/leireriel/" target="_blank" rel="noopener noreferrer" className="contact__link">
+                <i className="fab fa-codepen contact__link__icon"></i>
               </a>
             </li>
           </ol>
         </nav>
+        <p className="section__text contact__thanks"><small>Gracias por tu visita ❤</small></p>
       </div>
     );
   }
