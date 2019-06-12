ff93a2f8fd70bd558e285a14cf06c2bf3d63d434
leireriel <leirericop@gmail.com>
2019-06-12 09:58:25 +0200

    Create developer text

--- src/components/Developer/index.js
+++ src/components/Developer/index.js
@@ -1,11 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import './Developer.scss';
 
 class Developer extends Component {
   render() {
     return (
-      <Fragment>
-        <p>Developer</p>
-      </Fragment>
+      <div className="developer__container">
+        <div className="developer__description--container">
+          <h3 className="developer__description--title">Quién soy</h3>
+          <p className="developer__description--text">
+            ¡Hola! Me llamo Leire y me apasiona la programación <strong>front-end</strong>.
+            A la hora de programar, soy muy curiosa y me gusta probar cosas nuevas y entender cómo funcionan.
+            <strong>Me encanta aprender</strong>, ya sea haciendo pair programming o por mi cuenta, y siempre intento <strong>aportar sugerencias</strong>. 
+            Tengo gran capacidad de adaptabilidad y me siento muy motivada cuando puedo trabajar en <strong>proyectos nuevos</strong>.
+            Además, en mi código voy aplicando todo lo que aprendo sobre <strong>Clean Code :)</strong>
+          </p>
+        </div>
+      </div>
     );
   }
 }
