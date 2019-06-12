910642bac7580426eeb11c931fe587477e41da9a
leireriel <leirericop@gmail.com>
2019-06-12 10:40:54 +0200

    Add logos

--- src/components/Developer/index.js
+++ src/components/Developer/index.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import reactImg from './images/react.png';
+import javascriptImg from './images/javascript.png';
+import html5Img from './images/html5.png';
+import css3Img from './images/css3.png';
+import sassImg from './images/sass.png'
+import gitImg from './images/git.png';
+import npmImg from './images/npm.png';
+
 import './Developer.scss';
 
 class Developer extends Component {
@@ -14,6 +22,43 @@ class Developer extends Component {
             Tengo gran capacidad de adaptabilidad y me siento muy motivada cuando puedo trabajar en <strong>proyectos nuevos</strong>.
             Además, en mi código voy aplicando todo lo que aprendo sobre <strong>Clean Code :)</strong>
           </p>
+        </div>
+        <div className="developer__images--container">
+          <img 
+            src={reactImg} 
+            alt="Logo de librería React, Javascript"
+            className="developer__image"  
+          />
+          <img 
+            src={javascriptImg} 
+            alt="Logo de Javascript"
+            className="developer__image"  
+          />
+          <img 
+            src={html5Img} 
+            alt="Logo de HTML"
+            className="developer__image"  
+          />
+          <img 
+            src={css3Img} 
+            alt="Logo de CSS"
+            className="developer__image"  
+          />
+          <img 
+            src={sassImg} 
+            alt="Logo de SASS, SCSS"
+            className="developer__image"  
+          />
+          <img 
+            src={gitImg} 
+            alt="Logo de Git"
+            className="developer__image"  
+          />
+          <img 
+            src={npmImg} 
+            alt="Logo de NPM"
+            className="developer__image"  
+          />
         </div>
       </div>
     );
